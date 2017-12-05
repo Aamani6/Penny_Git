623cdae5dba5b8fd6abf0ddb2a8f59985261a997
12 <Lakshmi@DESKTOP-46TS987>
2017-12-05 13:48:12 -0600

    new code today

---
+++ Penny_project/src/test/java/com/NewTest/NewGitTest.java
@@ -0,0 +1,16 @@
+package com.NewTest;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NewGitTest {
+
+	public static void main(String[] args) {
+		
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.jcpenney.com/");
+	
+		
+
+   }
+}
